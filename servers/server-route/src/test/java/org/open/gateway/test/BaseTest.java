f9b7526f96720ee02beba68dd88b218355b1a503
miko <oni-miko@outlook.com>
2020-08-24 13:47:27 +0800

    1.提交代码

---
+++ servers/server-route/src/test/java/org/open/gateway/test/BaseTest.java
@@ -0,0 +1,31 @@
+package org.open.gateway.test;
+
+import org.springframework.util.StopWatch;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * Created by miko on 2020/8/20.
+ *
+ * @author MIKO
+ */
+public class BaseTest {
+
+    public static void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public StopWatch newStopWatch() {
+        return new StopWatch();
+    }
+
+    public void printSummary(String prefix, StopWatch sw) {
+        System.out.printf("%s 总耗时[%s ms] 次数[%s] 平均耗时[%s us].%n", prefix, sw.getTotalTimeMillis(), sw.getTaskCount(), BigDecimal.valueOf(sw.getTotalTimeNanos()).divide(BigDecimal.valueOf(sw.getTaskCount()), 2, RoundingMode.HALF_UP));
+    }
+
+}
---
+++ servers/server-route/src/test/java/org/open/gateway/test/TestSendLogs.java
@@ -0,0 +1,53 @@
+package org.open.gateway.test;
+
+import open.gateway.common.base.entity.AccessLogs;
+import open.gateway.common.utils.StringUtil;
+import org.junit.jupiter.api.Test;
+import org.open.gateway.route.GatewayRouteApplication;
+import org.open.gateway.route.service.AccessLogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StopWatch;
+
+import java.util.Date;
+
+/**
+ * Created by miko on 2020/8/20.
+ *
+ * @author MIKO
+ */
+@SpringBootTest(classes = GatewayRouteApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class TestSendLogs extends BaseTest {
+
+    @Autowired
+    private AccessLogsService accessLogsService;
+
+    @Test
+    public void testSendAccessLogs() {
+        StopWatch sw = newStopWatch();
+        for (int i = 0; i < 10000; i++) {
+            sw.start();
+            AccessLogs accessLogs = new AccessLogs();
+            accessLogs.setPath("/test/junit");
+            accessLogs.setIp("127.0.0.1");
+            accessLogs.setHttpStatus(HttpStatus.OK.value());
+            accessLogs.setHttpMethod("POST");
+            accessLogs.setHttpHeaders("Authorization: Bearer " + StringUtil.randomLetter(6) +
+                    "Content-Type: application/x-www-form-urlencoded");
+            accessLogs.setRequestQueryString(null);
+            accessLogs.setRequestBody("packs=" + StringUtil.randomNumber(6));
+            accessLogs.setRequestTime(new Date());
+            accessLogs.setResponseTime(new Date());
+            accessLogs.setUsedTime(100);
+            accessLogs.setUserAgent("Postman");
+            accessLogs.setRegion(null);
+            accessLogs.setError(null);
+            accessLogs.setApiCode(StringUtil.randomLetter(4));
+            accessLogs.setRouteCode(StringUtil.randomLetter(4));
+            accessLogsService.sendAccessLogs(accessLogs);
+            sw.stop();
+        }
+        printSummary("sendAccessLogs", sw);
+    }
+}
